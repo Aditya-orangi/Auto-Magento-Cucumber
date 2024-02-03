0d247d487933f4eaa235a2371652ab9c7955b50d
Aditya Das Choudhury <aditya.orangi@gmail.com>
2024-02-04 00:17:46 +0530

    Add files via upload
    Login Glue code added

---
+++ LoginStep.java
@@ -0,0 +1,32 @@
+package StepDefinition;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.And;
+public class LoginStep {
+
+//Glue Code, above imports not working
+@Given("user is on login page") 
+public void user_is_on_login_page() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java8.PendingException();
+}
+
+@When("user enters aditya.orangi@gmail.com and adc123ADC!")
+public void user_enters_creds(){
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java8.PendingException();
+}
+
+@And("clicks on login button")
+public void clicks_on_loginbutton(){
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java8.PendingException();
+}
+
+@Then("user is navigated to the home page")
+public void user_is_navigated_to_the_home_page(){
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java8.PendingException();
+}
+}
